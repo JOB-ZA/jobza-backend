4b02f04fb316e41146b64128fffd8d40e5508dbf
dgjinsu <rlawlstn3206@naver.com>
2023-09-29 00:23:06 +0900

    first commit

---
+++ src/test/java/jobza/JobzaApplicationTests.java
@@ -0,0 +1,13 @@
+package jobza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
