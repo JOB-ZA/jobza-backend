d3d0a6e68ee2f2a079b2ee4fc52a1f04264c37bc
dgjinsu <rlawlstn3206@naver.com>
2023-09-29 00:21:42 +0900

    feaat: 전역 예외처리 및 Errorcode 개발

---
+++ src/main/java/jobza/exception/CustomException.java
@@ -0,0 +1,17 @@
+package jobza.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private ErrorCode errorCode;
+    private String status;
+    private String errorMessage;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
+        this.errorCode = errorCode;
+        this.status = errorCode.getStatus().toString();
+        this.errorMessage = errorCode.getErrorMessage();
+    }
+}
