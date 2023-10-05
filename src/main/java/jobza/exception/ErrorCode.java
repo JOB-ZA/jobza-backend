c5e6e7dd18704d932b27fa3e449f83a638c654a9
dgjinsu <rlawlstn3206@naver.com>
2023-10-05 20:01:47 +0900

    feat: error code 추가

--- src/main/java/jobza/exception/ErrorCode.java
+++ src/main/java/jobza/exception/ErrorCode.java
@@ -15,7 +15,11 @@ public enum ErrorCode {
     REFRESH_TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "만료된 refresh token 입니다."),
     REFRESH_TOKEN_NOT_MATCH(HttpStatus.BAD_REQUEST, "잘못된 refresh token 요청 입니다."),
     REFRESH_TOKEN_UNKNOWN_ERROR(HttpStatus.BAD_REQUEST, "refresh token 재발급 중 알 수 없는 에러 발생."),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호 입니다.");
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호 입니다."),
+
+    FILE_NOT_VALID(HttpStatus.BAD_REQUEST, "파일이 없거나 잘못된 접근입니다."),
+    FILE_EXTENSION_NOT_VALID(HttpStatus.BAD_REQUEST, "pdf 파일 형식이 아닙니다."),
+    FILE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "pdf 파일 업로드 중 에러가 발생했습니다.");
 
 
     private final HttpStatus status;
