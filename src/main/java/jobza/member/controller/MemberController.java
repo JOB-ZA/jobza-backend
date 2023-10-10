4733ae6bea195e63b58362a3611ffc14e9dd8632
dgjinsu <rlawlstn3206@naver.com>
2023-10-10 23:28:13 +0900

    refactor: kakao oauth api redirect 주소 수정

--- src/main/java/jobza/member/controller/MemberController.java
+++ src/main/java/jobza/member/controller/MemberController.java
@@ -16,23 +16,21 @@ public class MemberController {
 
     private final OauthService oauthService;
 
-    @Operation(summary = "카카오 로그인", description = "카카오 로그인이라 아래 링크로 이동해서 로그인 테스트 진행 하면 됩니다<br>https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=de47672601f1318713d6ebbfca037682&redirect_uri=http://localhost:8080/login/oauth2/code/kakao")
+    @Operation(summary = "카카오 로그인", description = "카카오 로그인이라 아래 링크로 이동해서 로그인 테스트 진행 하면 됩니다<br>https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=de47672601f1318713d6ebbfca037682&redirect_uri=http://localhost:3000/auth")
     @GetMapping("/kakaoLogin")
     public void login() {
     }
 
 
-    @GetMapping("/login/oauth2/code/{registrationId}")
+    @GetMapping("/login/oauth2/code")
     public ResponseEntity<Response> redirect(
-            @PathVariable("registrationId") String registrationId
-            , @RequestParam("code") String code
+            @RequestParam("code") String code
             , @RequestParam(required = false ,value = "state") String state) {
         log.info("auth code 응답 완료: " + code);
-        log.info("state 응답 완료: " + state);
         return
                 oauthService.redirect(
                         TokenRequest.builder()
-                                .registrationId(registrationId)
+                                .registrationId("kakao")
                                 .code(code)
                                 .state(state)
                                 .build());
