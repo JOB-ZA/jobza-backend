f02ce9c5f53772a4b385f6bbcca0eaf875466d5e
dgjinsu <rlawlstn3206@naver.com>
2023-10-10 17:06:10 +0900

    fixed: jpa auditing 동작하지 않았던 문제 해결

--- src/main/java/jobza/JobzaApplication.java
+++ src/main/java/jobza/JobzaApplication.java
@@ -2,8 +2,10 @@ package jobza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JobzaApplication {
 
 	public static void main(String[] args) {
