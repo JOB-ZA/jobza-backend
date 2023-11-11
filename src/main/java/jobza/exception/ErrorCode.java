31fd06a17b82ad81aff6f15bc2ebcf7071ea1169
dgjinsu <rlawlstn3206@naver.com>
2023-11-11 22:05:13 +0900

    feat: 로드뷰 예외 코드 추가

--- src/main/java/jobza/exception/ErrorCode.java
+++ src/main/java/jobza/exception/ErrorCode.java
@@ -17,7 +17,9 @@ public enum ErrorCode {
     FILE_EXTENSION_NOT_VALID(HttpStatus.BAD_REQUEST, "pdf 파일 형식이 아닙니다."),
     FILE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "pdf 파일 업로드 중 에러가 발생했습니다."),
     RESUME_NOT_FOUND(HttpStatus.NOT_FOUND, "resume 데이터를 찾을 수 없습니다."),
-    MULTI_THREADING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "병렬 처리 과정 중 예외가 발생했습니다.");
+    MULTI_THREADING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "병렬 처리 과정 중 예외가 발생했습니다."),
+
+    KAKAO_API_NEED_ADDRESS(HttpStatus.BAD_REQUEST, "로드뷰로 띄우기 위해 요청된 주소가 없습니다.");
 
 
     private final HttpStatus status;
