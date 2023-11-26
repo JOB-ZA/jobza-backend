db2be75aa9c0a642855043bb18d0ab4e8cd407fa
Hyojinezz <gywlsdl668@naver.com>
2023-11-26 16:14:46 +0900

    d

---
+++ src/main/generated/jobza/skill/entity/QSkill.java
@@ -0,0 +1,61 @@
+package jobza.skill.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QSkill is a Querydsl query type for Skill
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QSkill extends EntityPathBase<Skill> {
+
+    private static final long serialVersionUID = 1057437549L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QSkill skill = new QSkill("skill");
+
+    public final jobza.common.QBaseEntity _super = new jobza.common.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> lastModifiedDate = _super.lastModifiedDate;
+
+    public final jobza.member.entity.QMember member;
+
+    public final StringPath name = createString("name");
+
+    public QSkill(String variable) {
+        this(Skill.class, forVariable(variable), INITS);
+    }
+
+    public QSkill(Path<? extends Skill> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QSkill(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QSkill(PathMetadata metadata, PathInits inits) {
+        this(Skill.class, metadata, inits);
+    }
+
+    public QSkill(Class<? extends Skill> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.member = inits.isInitialized("member") ? new jobza.member.entity.QMember(forProperty("member")) : null;
+    }
+
+}
+
